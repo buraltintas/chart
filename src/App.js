44056e769d1b58099c3c690e219d0f6d7a9db20f
Burak Altıntaş <burak.altintas@yahoo.com.tr>
2021-12-23 11:57:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TotalCardChart from "./components/TotalCardChart";
+import classes from "../src/App.module.css";
+import CardChart from "./components/CardChart";
+import AccChart from "./components/AccChart";
 
 function App() {
+  const [showTotalCard, setShowTotalCard] = useState(true);
+  const [showCard, setShowCard] = useState(false);
+  const [showAcc, setShowAcc] = useState(false);
+  const [showCardActive, setShowCardActive] = useState(false);
+  const [totalCardActive, setTotalCardActive] = useState(true);
+  const [showAccActive, setShowAccActive] = useState(false);
+
+  const showTotalCardHandler = () => {
+    setShowAcc(false);
+    setShowTotalCard(true);
+    setShowCard(false);
+    setTotalCardActive(true);
+    setShowCardActive(false);
+    setShowAccActive(false);
+  };
+
+  const showCardHandler = () => {
+    setShowAcc(false);
+    setShowCard(true);
+    setShowTotalCard(false);
+    setTotalCardActive(false);
+    setShowCardActive(true);
+    setShowAccActive(false);
+  };
+
+  const showAccHandler = () => {
+    setShowAcc(true);
+    setShowCard(false);
+    setShowTotalCard(false);
+    setTotalCardActive(false);
+    setShowCardActive(false);
+    setShowAccActive(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className={classes.header}>
+        <div className={classes.logo}>
+          <h1>Kredi Kartları ve Hesap Hareketleri</h1>
+        </div>
+        <ul className={classes.navButtons}>
+          <li
+            onClick={showTotalCardHandler}
+            className={`${totalCardActive ? "active" : ""}`}
+          >
+            Kart Toplam Harcamalar
+          </li>
+          <li
+            onClick={showCardHandler}
+            className={showCardActive ? "active" : ""}
+          >
+            Kart Bazında Harcamalar
+          </li>
+          <li
+            onClick={showAccHandler}
+            className={showAccActive ? "active" : ""}
+          >
+            Hesap Hareketleri
+          </li>
+        </ul>
+      </nav>
+      {showTotalCard && !showCard && !showAcc && <TotalCardChart />}
+      {!showTotalCard && showCard && <CardChart />}
+      {!showTotalCard && !showCard && showAcc && <AccChart />}
     </div>
   );
 }
---
+++ src/components/AccChart.js
@@ -0,0 +1,7 @@
+import classes from "./AccChart.module.css";
+
+const AccChart = () => {
+  return <div>hesap</div>;
+};
+
+export default AccChart;
